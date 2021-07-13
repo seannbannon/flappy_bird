ab33c9f88d10191cef34bc85f9bd89373b4228e8
Sean Bannon <seannbannon@gmail.com>
2021-07-13 11:08:50 -0500

    set up gravity

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let birdLeft = 220
     let birdBottom = 100
+    let gravity = 2
 
     function startGame(){
+        birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft
+        bird.style.left = birdLeft + 'px'
     }
-    startGame()
+    let timerId = setInterval(startGame, 20)
+
+    // making our bird jump, 1 hour 28 sex
 
 })
