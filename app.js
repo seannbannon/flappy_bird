83ab6c1f138f22bc078a966db0f668bbad7ac4bb
Sean Bannon <seannbannon@gmail.com>
2021-08-01 12:44:06 -0400

    obstacle moves

--- app.js
+++ app.js
@@ -29,9 +29,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function generateObstacle() {
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
         obstacle.classList.add('obstacle')
         gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle(){
+            obstacleLeft -=2
+            obstacle.style.left = obstacleLeft + 'px'
+        }
+        let timerId = setInterval(moveObstacle, 20)
     }
     generateObstacle()
 
